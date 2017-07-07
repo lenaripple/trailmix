d393f876d7ba0e30a4481dd5b53dcdd97ec099ca
Lena Ripple <ripplelena@gmail.com>
2017-07-07 13:16:58 -0600

    pre-auth0 commit

--- App/Components/Login.js
+++ App/Components/Login.js
@@ -15,59 +15,6 @@ class Login extends Component {
   static navigationOptions = {
     title: 'Login Page',
   };
-  // state = {
-  //   user: undefined, // user has not logged in yet
-  // };
-  //
-  // // Set up Linking
-  // componentDidMount() {
-  //   // Add event listener to handle OAuthLogin:// URLs
-  //   Linking.addEventListener('url', this.handleOpenURL);
-  //   // Launched from an external URL
-  //   Linking.getInitialURL().then((url) => {
-  //     if (url) {
-  //       this.handleOpenURL({ url });
-  //     }
-  //   });
-  // };
-  //
-  // componentWillUnmount() {
-  //   // Remove event listener
-  //   Linking.removeEventListener('url', this.handleOpenURL);
-  // };
-  //
-  // handleOpenURL = ({ url }) => {
-  //   // Extract stringified user string out of the URL
-  //   const [, user_string] = url.match(/user=([^#]+)/);
-  //   this.setState({
-  //     // Decode the user string and parse it into JSON
-  //     user: JSON.parse(decodeURI(user_string))
-  //   });
-  //   if (Platform.OS === 'ios') {
-  //     SafariView.dismiss();
-  //   }
-  // };
-  //
-  // // Handle Login with Facebook button tap
-  // loginWithFacebook = () =>
-  //   this.openURL('http://localhost:3000/auth/facebook');
-  //
-  // openURL = (url) => {
-  //   console.log(url);
-  //   console.log(SafariView);
-  //   // Use SafariView on iOS
-  //   if (Platform.OS === 'ios') {
-  //     SafariView.show({
-  //       url: url,
-  //       fromBottom: true,
-  //     });
-  //   }
-  //   // Or Linking.openURL on Android
-  //   else {
-  //     Linking.openURL(url);
-  //   }
-  // };
-
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -83,44 +30,9 @@ class Login extends Component {
           <Text style={styles.buttonText}>Get Started</Text>
       </TouchableHighlight>
     </View>
-      // <View style={styles.container}>
-      //   { user
-      //     ? // Show user info if already logged in
-      //       <View style={styles.content}>
-      //         <Text style={styles.header}>
-      //           Welcome {user.name}!
-      //         </Text>
-      //         <View style={styles.avatar}>
-      //           <Image source={{ uri: user.avatar }} style={styles.avatarImage} />
-      //         </View>
-      //       </View>
-      //     : // Show Please log in message if not
-      //       <View style={styles.content}>
-      //         <Text style={styles.header}>
-      //           Welcome to trailMix!
-      //         </Text>
-      //         <Text style={styles.text}>
-      //           Please log in to continue
-      //         </Text>
-      //       </View>
-      //   }
-      //   <View style={styles.buttons}>
-      //     <Button
-      //       name="facebook"
-      //       backgroundColor="#3b5998"
-      //       onPress={this.loginWithFacebook}
-      //       title="Login with Facebook"
-      //     />
-      //   </View>
-      // </View>
     );
   }
 }
-
-// const iconStyles = {
-//   borderRadius: 10,
-//   iconStyle: { paddingVertical: 5 },
-// };
 
 const styles = StyleSheet.create({
   lightGreen: {
